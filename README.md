182bf0b00bc500ead090fe46e872c27fef59e27c
Milton Boos Junior <junior_boos@live.com>
2023-03-09 03:33:24 +0000

    add markdown badges

---
+++ README.md
@@ -0,0 +1,7 @@
+# About me
+
+- 😃 I'm a cool guy
+
+# Tech Stack
+
+![Dependabot](https://img.shields.io/badge/dependabot-025E8C?style=for-the-badge&logo=dependabot&logoColor=white) ![Blogger](https://img.shields.io/badge/Blogger-FF5722?style=for-the-badge&logo=blogger&logoColor=white)
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { FormEvent, useState } from "react";
 import {
   Button,
   Box,
@@ -8,6 +8,8 @@ import {
 } from "@primer/react";
 import { useInputHandler } from "./hooks/useInputHandler";
 import EmojiPicker from "emoji-picker-react";
+import { BadgesProps, markdownBadges } from "./utils/markdownBadges";
+import Select, { MultiValue } from "react-select";
 
 function App() {
   const {
@@ -20,56 +22,100 @@ function App() {
     handleShowEmojis,
   } = useInputHandler({ text: "", emoji: "😃" });
 
+  const [selectedBadges, setSelectedBadges] = useState<MultiValue<BadgesProps>>(
+    []
+  );
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
 
   return (
     <ThemeProvider>
-      <Box as="form" onSubmit={handleSubmit}>
-        {inputList.map((singleInput, idx) => (
-          <FormControl key={idx}>
-            <FormControl.Label>Label</FormControl.Label>
-            <Box display="flex" sx={{ gap: "0.5rem", marginBottom: "1rem" }}>
-              <TextInput
-                placeholder="Text"
-                value={singleInput.text}
-                onChange={(e) => handleInputChange(e, idx)}
-              />
-              <Box sx={{ position: "relative" }}>
-                <Button onClick={() => handleShowEmojis(idx)}>
-                  {singleInput.emoji}
-                </Button>
-                {isEmojisShown === idx && (
-                  <Box
-                    sx={{
-                      position: "absolute",
-                      top: "100%",
-                      left: 0,
-                      zIndex: 2,
-                    }}
-                  >
-                    <EmojiPicker
-                      onEmojiClick={(e) => handleEmojiChange(e.emoji, idx)}
-                    />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          padding: "2rem",
+        }}
+      >
+        <Box
+          as="form"
+          onSubmit={handleSubmit}
+          sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+        >
+          <Box>
+            {inputList.map((singleInput, idx) => (
+              <FormControl key={idx}>
+                <FormControl.Label>Description</FormControl.Label>
+                <Box
+                  display="flex"
+                  sx={{ gap: "0.5rem", marginBottom: "1rem" }}
+                >
+                  <TextInput
+                    placeholder="Text"
+                    value={singleInput.text}
+                    onChange={(e) => handleInputChange(e, idx)}
+                  />
+                  <Box sx={{ position: "relative" }}>
+                    <Button onClick={() => handleShowEmojis(idx)}>
+                      {singleInput.emoji}
+                    </Button>
+                    {isEmojisShown === idx && (
+                      <Box
+                        sx={{
+                          position: "absolute",
+                          top: "100%",
+                          left: 0,
+                          zIndex: 2,
+                        }}
+                      >
+                        <EmojiPicker
+                          onEmojiClick={(e) => handleEmojiChange(e.emoji, idx)}
+                        />
+                      </Box>
+                    )}
                   </Box>
-                )}
-              </Box>
 
-              <Button onClick={() => handleInputRemove(idx)}>Remove</Button>
-            </Box>
+                  <Button onClick={() => handleInputRemove(idx)}>Remove</Button>
+                </Box>
+              </FormControl>
+            ))}
+            <Button onClick={() => handleInputAdd()}>Add more</Button>
+          </Box>
+          <FormControl>
+            <FormControl.Label>TextInputWithTokens</FormControl.Label>
+            <Select
+              isMulti
+              name="markdown-badges"
+              options={markdownBadges}
+              styles={{ container: (base) => ({ ...base, width: 500 }) }}
+              value={selectedBadges}
+              onChange={(badges) => setSelectedBadges(badges)}
+            />
           </FormControl>
-        ))}
+        </Box>
+        <hr style={{ width: "100%" }} />
+        <Box>
+          {inputList.length > 0 && (
+            <div>
+              <p># About me</p>
+              {inputList.map((singleInput, idx) => (
+                <p key={idx}>
+                  - {` ${singleInput.emoji}`} {` ${singleInput.text}`}
+                </p>
+              ))}
+            </div>
+          )}
 
-        <Button onClick={() => handleInputAdd()}>Add more</Button>
-      </Box>
-      <Box>
-        <p># About me</p>
-        {inputList.map((singleInput, idx) => (
-          <p key={idx}>
-            - {` ${singleInput.emoji}`} {` ${singleInput.text}`}
+          <p># Tech Stack</p>
+          <p>
+            {selectedBadges.map((badge) => (
+              <span>{badge.value} </span>
+            ))}
           </p>
-        ))}
+        </Box>
       </Box>
     </ThemeProvider>
   );
