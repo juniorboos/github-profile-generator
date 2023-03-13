020b9dc77d7ed63041c8a5195f756bd3d9640b75
Milton Boos Junior <junior_boos@live.com>
2023-03-13 16:58:48 +0000

    change markdown library

--- src/components/MarkdownPreview.tsx
+++ src/components/MarkdownPreview.tsx
@@ -1,13 +1,10 @@
-import { Box } from "@primer/react";
-import { MarkdownEditor } from "@primer/react/drafts";
+import { Box, IconButton } from "@primer/react";
 import { BiCopy } from "@react-icons/all-files/bi/BiCopy";
 import { BiCheck } from "@react-icons/all-files/bi/BiCheck";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import rehypeSanitize from "rehype-sanitize";
-import rehypeRaw from "rehype-raw";
+import { BsEye } from "@react-icons/all-files/bs/BsEye";
+import { BsEyeSlash } from "@react-icons/all-files/bs/BsEyeSlash";
 import { useState } from "react";
-import ReactDomServer from "react-dom/server";
+import MDEditor from "@uiw/react-md-editor";
 
 interface MarkdownPreviewProps {
   onCopy: () => void;
@@ -25,53 +22,29 @@ const MarkdownPreview = ({ onCopy, markdown }: MarkdownPreviewProps) => {
     setTimeout(() => setIsClicked(false), 3000);
   };
 
-  const renderMarkdown = async (markdown: string) =>
-    ReactDomServer.renderToString(
-      <ReactMarkdown
-        children={markdown}
-        remarkPlugins={[remarkGfm]}
-        rehypePlugins={[rehypeRaw, rehypeSanitize]}
-        components={{
-          a: ({ node, ...props }) => (
-            <a className="inline-block w-fit" {...props} />
-          ),
-          img: ({ node, ...props }) => (
-            <img
-              className={
-                props.alt === "GitHub Stats" ? "w-full" : "w-auto inline-block"
-              }
-              {...props}
-            />
-          ),
-          ul: ({ node, ...props }) => (
-            <ul className="list-disc list-inside" {...props}></ul>
-          ),
-        }}
-      />
-    );
-
   return (
-    <Box className="relative p-4 flex-grow">
-      <MarkdownEditor
+    <Box className="relative p-4 flex-grow min-h-full">
+      <MDEditor
         value={markdown}
-        onChange={() => {}}
-        onRenderPreview={renderMarkdown}
-        viewMode={isEditing ? "edit" : "preview"}
-        onChangeViewMode={(viewMode) => setIsEditing(viewMode === "edit")}
-      >
-        <MarkdownEditor.Label>Markdown</MarkdownEditor.Label>
-        <MarkdownEditor.Toolbar></MarkdownEditor.Toolbar>
-        <MarkdownEditor.Actions>
-          <MarkdownEditor.ActionButton
-            trailingIcon={isClicked ? BiCheck : BiCopy}
-            aria-label="Copy markdown"
-            onClick={handleClick}
-            size="large"
-          >
-            Copy Markdown
-          </MarkdownEditor.ActionButton>
-        </MarkdownEditor.Actions>
-      </MarkdownEditor>
+        hideToolbar
+        preview={isEditing ? "edit" : "preview"}
+        className="min-h-full"
+        visibleDragbar={false}
+      />
+      <Box className="absolute top-6 right-9 flex gap-2">
+        <IconButton
+          icon={isEditing ? BsEyeSlash : BsEye}
+          aria-label={`Go to ${isEditing ? "preview" : "edit"} mode`}
+          onClick={() => setIsEditing((prevIsEditing) => !prevIsEditing)}
+          size="large"
+        />
+        <IconButton
+          icon={isClicked ? BiCheck : BiCopy}
+          aria-label="Copy markdown"
+          onClick={handleClick}
+          size="large"
+        />
+      </Box>
     </Box>
   );
 };
