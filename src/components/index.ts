eab93425b3be3391ef5e484377a559d590aaf2c6
Milton Boos Junior <junior_boos@live.com>
2023-03-16 16:11:57 +0000

    change emoji-picker component to folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export * from "./about-me";
 export * from "./contact-me";
-export * from "./EmojiPicker";
+export * from "./emoji-picker";
 export * from "./GithubStats";
 export * from "./Inputs";
 export * from "./MarkdownPreview";
