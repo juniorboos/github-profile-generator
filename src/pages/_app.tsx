c1f5bea4751fa898e6f161bb4e8cddb0f6b1ac63
Milton Boos Junior <junior_boos@live.com>
2023-03-20 23:10:49 +0000

    fix flashy styles from styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../index.css";
 import type { AppProps } from "next/app";
-import { SSRProvider, ThemeProvider } from "@primer/react";
+import { BaseStyles, SSRProvider, ThemeProvider } from "@primer/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SSRProvider>
       <ThemeProvider colorMode="night">
-        <Component {...pageProps} />
+        <BaseStyles>
+          <Component {...pageProps} />
+        </BaseStyles>
       </ThemeProvider>
     </SSRProvider>
   );
